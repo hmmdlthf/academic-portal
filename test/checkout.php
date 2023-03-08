34da4150289c903146eb9f16aa28af774f3431c7
kevin_jorn <Althaf>
2023-03-08 22:35:51 +0530

    modify local

--- test/checkout.php
+++ test/checkout.php
@@ -13,13 +13,14 @@ $amount = 1000.00;
 $currency = 'LKR';
 // $merchant_secret = 'MzA4NTY0Mzg5NjEwOTA4NTg0OTkyMjQ1NDg2MzU0MzEyOTE1ODAzMA=='; // secret for localhost
 $merchant_secret = 'Njc4ODA0NDMzNjI1NjUwNDIyNTc1Njg2NDMzNDIzMTg1MzMy'; // scret for aftersite
+
 // $return_url = 'http://localhost:3000/test/checkoutSuccess.php';
 // $cancel_url = 'http://localhost:3000/test/checkoutCancel.php';
-// $notify_url = 'http://localhost:3000/test/checkoutNotify.php';
+// $notify_url = 'https://academicportal.aftersite.lk/test/checkoutNotify.php';
 
-$return_url = 'https://academicportal.aftersite.lk/checkoutSuccess.php';
-$cancel_url = 'https://academicportal.aftersite.lk/checkoutCancel.php';
-$notify_url = 'https://academicportal.aftersite.lk/checkoutNotify.php';
+$return_url = 'https://academicportal.aftersite.lk/test/checkoutSuccess.php';
+$cancel_url = 'https://academicportal.aftersite.lk/test/checkoutCancel.php';
+$notify_url = 'https://academicportal.aftersite.lk/test/checkoutNotify.php';
 
 
 $paymentService = new PaymentService();
